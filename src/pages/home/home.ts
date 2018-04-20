6fe32f9990f8fc75855efa78a7aac9610461c39b
flarrea <flarreas@gmail.com>
2018-04-20 17:37:19 -0400

    app demoionic v3

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RestApiProvider } from '../../providers/rest-api/rest-api';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,27 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  cajeros: string[];
+  errorMessage: string;
+  descending: boolean = false;
+  order: number;
+  column: string = 'direccion';
 
+  constructor(public navCtrl: NavController, public rest: RestApiProvider) {}
+
+  ionViewDidLoad() {
+    this.getCajeros();
   }
 
+  getCajeros() {
+    this.rest.getCajeros()
+       .subscribe(
+         cajeros => this.cajeros = cajeros,
+         error =>  this.errorMessage = <any>error);
+  }
+
+  sort(){
+    this.descending = !this.descending;
+    this.order = this.descending ? 1 : -1;
+  }
 }
